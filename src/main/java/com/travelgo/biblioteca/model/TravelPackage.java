bf513d8b87a38647d2e6fd0d45e4e771ebfe20fd
ExilRaizen <italocampodonico21@gmail.com>
2026-04-22 22:29:52 -0400

    Detalle olvidado

--- src/main/java/com/travelgo/biblioteca/model/TravelPackage.java
+++ src/main/java/com/travelgo/biblioteca/model/TravelPackage.java
@@ -1,11 +1,13 @@
 package com.travelgo.biblioteca.model; // Coincide con la carpeta 'model'
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class TravelPackage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/travelgo/biblioteca/model/User.java
+++ src/main/java/com/travelgo/biblioteca/model/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Entity
 @Table(name = "app_users") // Evita conflicto con la tabla 'user' de H2
 @Data // Para getters y setters automaticamente
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue
