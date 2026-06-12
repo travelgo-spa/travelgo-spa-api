a4109233491e37d3378d27f11a01c3681c3ee40b
Felipedegoyeneche <fe.degoyeneche@duocuc.cl>
2026-06-12 16:57:23 -0400

    tags agregados

--- travelgo-spa-api/src/main/java/com/travelgo/biblioteca/controller/UserController.java
+++ travelgo-spa-api/src/main/java/com/travelgo/biblioteca/controller/UserController.java
@@ -2,6 +2,8 @@ package com.travelgo.biblioteca.controller;
 
 import com.travelgo.biblioteca.model.User;
 import com.travelgo.biblioteca.service.UserService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,10 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "*")
+@Tag(
+    name = "Usuarios",
+    description = "Endpoints para listar, buscar, crear y eliminar usuarios"
+)
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
