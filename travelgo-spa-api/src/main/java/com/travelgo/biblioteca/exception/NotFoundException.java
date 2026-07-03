40f0d517910d82e1dbacd1d821fa381c42b2551b
Felipedegoyeneche <fe.degoyeneche@duocuc.cl>
2026-07-03 15:02:04 -0400

    Delete travelgo-spa-api directory

--- travelgo-spa-api/src/main/java/com/travelgo/biblioteca/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.travelgo.biblioteca.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
